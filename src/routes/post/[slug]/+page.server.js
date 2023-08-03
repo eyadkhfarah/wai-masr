543341c4e55e1fb852393b134b26d6abd3e2d0ae
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-08-04 01:26:37 +0300

    try and ctach

--- src/routes/post/[slug]/+page.server.js
+++ src/routes/post/[slug]/+page.server.js
@@ -1,23 +1,32 @@
 import contentful from 'contentful';
-import { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_KEY, NEXT_PUBLIC_DOMAIN_URL } from '$env/static/private';
+import {
+	CONTENTFUL_SPACE_ID,
+	CONTENTFUL_ACCESS_KEY,
+	NEXT_PUBLIC_DOMAIN_URL
+} from '$env/static/private';
 
 export async function load({ params }) {
-	const client = contentful.createClient({
-		space: CONTENTFUL_SPACE_ID,
-		accessToken: CONTENTFUL_ACCESS_KEY
-	});
+	try {
+		const client = contentful.createClient({
+			space: CONTENTFUL_SPACE_ID,
+			accessToken: CONTENTFUL_ACCESS_KEY
+		});
 
-	const { items } = await client.getEntries({
-		content_type: 'news',
-		'fields.slug': params.slug
-	});
-	
-	// const res = await client.getEntries({ content_type: 'news' });
+		const { items } = await client.getEntries({
+			content_type: 'news',
+			'fields.slug': params.slug
+		});
 
-	if (items) {
-		return {
-			article: items[0],
-			// cards: res.items
-		};
+		// const res = await client.getEntries({ content_type: 'news' });
+
+		if (items) {
+			return {
+				article: items[0],
+				status: 200
+				// cards: res.items
+			};
+		}
+	} catch (error) {
+		return new Response(JSON.stringify(error));
 	}
 }
