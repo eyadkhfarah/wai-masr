bb8097fe9a20233d4b23db6f965ea6676a146cbd
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-08-05 03:53:16 +0300

    add sitemap

--- src/routes/events/[slug]/+page.server.js
+++ src/routes/events/[slug]/+page.server.js
@@ -1,4 +1,21 @@
-// /** @type {import('./$types').PageServerLoad} */
-// export async function load() {
-//     return {};
-// };
+import contentful from 'contentful';
+import {
+	CONTENTFUL_SPACE_ID,
+	CONTENTFUL_ACCESS_KEY,
+} from '$env/static/private';
+
+export async function load({ params }) {
+	const client = contentful.createClient({
+		space: CONTENTFUL_SPACE_ID,
+		accessToken: CONTENTFUL_ACCESS_KEY
+	});
+
+	const { items } = await client.getEntries({
+		content_type: 'events',
+		'fields.slug': params.slug
+	});
+
+	return {
+		event: items[0],
+	};
+}
---
+++ src/routes/sitemap.xml/+server.js
@@ -0,0 +1,64 @@
+import { categories } from '../../utils/categories';
+import { menu } from '../../utils/navLink';
+let site = 'https://wai-masr.vercel.app';
+
+import {client} from '../../lib/contentful-fetch'
+
+
+export async function GET() {
+
+    const res = await client.getEntries({ content_type: 'news' });
+    const res1 = await client.getEntries({ content_type: 'events' });
+
+    const article = res.items
+    const events = res1.items
+    
+    return new Response(
+		`
+        <?xml version="1.0" encoding="UTF-8" ?>
+        <urlset
+            xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+            xmlns:xhtml="https://www.w3.org/1999/xhtml"
+            xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+            xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+            xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+            xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+        >
+        ${menu.filter((link) => link.link.indexOf("https://maktabaqawmya.vercel.app")).map((page) => 
+					`<url>
+            <loc>${site}${page.link}</loc>
+            <changefreq>daily</changefreq>
+            <priority>0.7</priority>
+          </url>`
+				).join('')}
+        ${categories.filter((link) => link.link.indexOf("https://maktabaqawmya.vercel.app")).map((page) => 
+					`<url>
+            <loc>${site}/article/${page.link}</loc>
+            <changefreq>daily</changefreq>
+            <priority>0.7</priority>
+          </url>`
+				).join('')}
+        ${article.map((page) => 
+					`<url>
+            <loc>${site}/post/${page.fields.slug}</loc>
+            <changefreq>daily</changefreq>
+            <priority>0.7</priority>
+          </url>`
+				).join('')}
+        ${events.map((page) => 
+					`<url>
+            <loc>${site}/events/${page.fields.slug}</loc>
+            <changefreq>daily</changefreq>
+            <priority>0.7</priority>
+          </url>`
+				).join('')}
+        </urlset>`.trim(),
+		{
+			headers: {
+				'Content-Type': 'application/xml'
+			}
+		}
+
+        
+	);
+}
