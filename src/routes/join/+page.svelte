f30f117e18783861bdcb033834384c32370557e4
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-08-10 01:36:57 +0300

    job description

--- src/routes/join/+page.svelte
+++ src/routes/join/+page.svelte
@@ -43,38 +43,55 @@
 		<div class="lg:grid flex gap-9 mb-3 lg:place-items-center bg-white p-4 w-full">
 			<img src={designer} alt="graphic designer" class="lg:h-28 h-16 w-fit" />
 			<div class="grid">
-				<h2 class="m-0 text-red">جرافيك ديزاينر</h2>
+				<h2 class="m-0 text-red lg:text-center">جرافيك ديزاينر</h2>
+				<p class="my-3 text-xs lg:text-center">
+					لو إنت مصمم جرافيك وعندك شغل بمواضيع القومية والهوية المصرية ومتمكن من الفوتوشوب خصوصًا
+					للسوشيال ميديا تقدر تنضم لينا.
+				</p>
 			</div>
 		</div>
 		<div class="lg:grid flex gap-9 mb-3 lg:place-items-center bg-white p-4 w-full">
 			<img src={cms} alt="cms" class="w-fit lg:h-28 h-16" />
 			<div class="grid">
-				<h2 class="m-0 text-red">إدارة الموقع</h2>
+				<h2 class="m-0 text-red lg:text-center">إدارة الموقع</h2>
+				<p class="my-3 text-xs lg:text-center">لو إنت في كارير السوشيال ميديا ولسه بتبدأ فعندك فرصة عظيمة لتطوير مهاراتك وإمكانياتك في منصات وعي - مصر بالكامل.</p>
 			</div>
 		</div>
 		<div class="lg:grid flex gap-9 mb-3 lg:place-items-center bg-white p-4 w-full">
 			<img src={writer} alt="writer" class="w-fit lg:h-28 h-16" />
 			<div class="grid">
-				<h2 class="m-0 text-red">كتابة محتوي</h2>
+				<h2 class="m-0 text-red lg:text-center">كتابة محتوي</h2>
+				<p class="my-3 text-xs lg:text-center">
+					حابب تمارس حبك وهوايتك لكتابة المحتوى وتاخد خبرة إضافية في أحد أبرز المنصات الصاعدة علي
+					السوشيال ميديا وكمان تكسب مهارات جديدة لجذب اهتمام المتابعين؟ يبقى الفريق ده ليك.
+				</p>
 			</div>
 		</div>
 		<div class="lg:grid flex gap-9 mb-3 lg:place-items-center bg-white p-4 w-full">
 			<img src={editing} alt="editior" class="w-fit lg:h-28 h-16" />
 			<div class="grid">
-				<h2 class="m-0 text-red">مونتاج</h2>
+				<h2 class="m-0 text-red lg:text-center">مونتاج</h2>
+				<p class="my-3 text-xs lg:text-center">
+					عندك مهارات وموهبة المونتاج للفيديوهات وحابب يكون مكان بيتيحلك حرية الإبداع في التنفيذ
+					وكمان فرصة لنجاح عظيم في صناعة المحتوى؟ قدم في الفريق وابدأ.
+				</p>
 			</div>
 		</div>
 		<div class="lg:grid flex gap-9 mb-3 lg:place-items-center bg-white p-4 w-full">
 			<img src={reporter} alt="presenter" class="w-fit lg:h-28 h-16" />
 			<div class="grid">
-				<h2 class="m-0 text-red">تقديم</h2>
+				<h2 class="m-0 text-red lg:text-center">تقديم</h2>
+				<p class="my-3 text-xs lg:text-center">
+					عندك مهارات الالقاء والكاريزما والقبول؟ ومحتاج فرصة عظيمة تنمي فيها موهبتك وتاخد إمكانية
+					حقيقية للظهور لجمهور عظيم؟ قدم دلوقتي.
+				</p>
 			</div>
 		</div>
 	</div>
 </section>
 
 <section class="grid place-items-center">
-	<h2>مستني ايه تعالي انضم معانا دلوقتي</h2>
+	<h2 class="text-center">مستني ايه تعالي انضم معانا دلوقتي</h2>
 	<a class="Blackbtn" href="https://forms.gle/weNzsctgZPJKHSb5A" rel="noopener noreferrer"
 		>قدم دلوقتي<span><Icon color="red" src={RiSystemArrowLeftLine} /></span></a
 	>
