10c9d4721780de94141b6fe942263613167f176b
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-08-09 23:01:28 +0300

    add top buton

---
+++ src/routes/post/[slug]/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+
+	let scrollPrecent = '0';
+
+	function updateProgressBar() {
+		let precent = (document.body.scrollTop || document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100 + "%"
+
+        return precent = scrollPrecent
+	}
+
+</script>
+
+<svelte:window on:scroll={updateProgressBar} />
+
+<div class={`h-2 w-[${scrollPrecent}] bg-red transition-all ease-in-out duration-300 fixed lg:top-12 md:top-[38px] z-10`} />
+<slot />
