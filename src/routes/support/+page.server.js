9dc7ca0b5f11d028d1c4a0ab30f91ff50849e313
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-08-12 00:01:23 +0300

    test paypal

--- src/routes/support/+page.server.js
+++ src/routes/support/+page.server.js
@@ -1,5 +1,11 @@
+import {PAYPAL_CLIENT_SERCET} from "$env/static/private"
+
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
+
+    let env = PAYPAL_CLIENT_SERCET
     
-    return {};
+    return {
+        env
+    };
 };
--- src/routes/support/+page.svelte
+++ src/routes/support/+page.svelte
@@ -1,7 +1,10 @@
 <script>
 	// @ts-nocheck
 
-	import Donate from '../../lib/Components/Donate.svelte';
+	export let data
+
+	let env = data.env
+
 	import H1 from '../../lib/Components/Text/H1.svelte';
 
 	import og from '../../lib/images/main.png';
@@ -9,42 +12,53 @@
 	let title = 'وعي مصر :: أدعمنا';
 	let desc = 'ادعم فريق وعي مصر ماديا';
 
-	let amount = 50;
+	let amount = 1.62;
+
+	console.log(amount);
 
 	import { loadScript } from '@paypal/paypal-js';
 
-	const CLIENT_ID = import.meta.env.PAYPAL_CLIENT_API;
+	console.log(env)
 
-	// loadScript({ 'client-id': CLIENT_ID }).then((paypal) => {
-	// 	paypal.Buttons({
-	// 		style: {
-	// 			color: 'blue',
-	// 			shape: 'pill'
-	// 		},
-	// 		createOrder: function (data, actions) {
-	// 			// Set up the transaction
-	// 			return actions.order.create({
-	// 				purchase_units: [
-	// 					{
-	// 						amount: {
-	// 							value: amount
-	// 						}
-	// 					}
-	// 				]
-	// 			});
-	// 		},
-	// 		onApprove: async function (data, actions) {
-	// 			// Capture order after payment approved
-	// 			const details = await actions.order.capture();
-	// 			alert('Payment successful!');
-	// 		},
-	// 		onError: function (err) {
-	// 			// Log error if something goes wrong during approval
-	// 			alert('Something went wrong');
-	// 			console.log('Something went wrong', err);
-	// 		}
-	// 	}).render("#paypal-button-container");
-	// });
+	loadScript({ clientId: env })
+		.then((paypal) => {
+			paypal
+				.Buttons({
+					style: {
+						color: 'blue',
+						shape: 'pill'
+					},
+					createOrder: function (data, actions) {
+						// Set up the transaction
+						return actions.order.create({
+							purchase_units: [
+								{
+									amount: {
+										value: amount
+									}
+								}
+							]
+						});
+					},
+					onApprove: async function (data, actions) {
+						// Capture order after payment approved
+						const details = await actions.order.capture();
+						alert('Payment successful!');
+					},
+					onError: function (err) {
+						// Log error if something goes wrong during approval
+						alert('Something went wrong');
+						console.log('Something went wrong', err);
+					}
+				})
+				.render('#paypal-button-container')
+				.catch((error) => {
+					console.error('failed to render the PayPal Buttons', error);
+				});
+		})
+		.catch((error) => {
+			console.error('failed to load the PayPal JS SDK script', error);
+		});
 
 	// @ts-ignore
 </script>
@@ -58,7 +72,7 @@
 </svelte:head>
 
 <section class="md:grid lg:grid-cols-2 gap-7">
-	<div class="grid gap-3">
+	<div class="grid gap-3 h-fit">
 		<H1>أدعمنا</H1>
 		<p>حابب اننا نكبر ونكبر معاك؟ تقجر تتبرع لنا دلوقتي</p>
 	</div>
@@ -66,29 +80,35 @@
 	<div class="bg-red grid gap-4 text-sm p-4 md:m-0 mt-5 border-2">
 		<div class="flex gap-4 justify-around">
 			<button
-				class:text-black={amount === 10}
+				class:text-black={amount === 0.32}
 				class={`p-4 h-fit w-fit font-black text-lg cursor-pointer ${
-					amount === 10 ? 'bg-primary' : ''
+					amount === 0.32 ? 'bg-primary' : ''
 				} `}
-				on:click={() => (amount = 10)}
+				on:click={() => amount = 0.32}
 			>
 				10 جنيه
 			</button>
 			<button
-				class:text-black={amount === 50}
+				on:click={() => {
+					amount = 1.62;
+				}}
+				class:text-black={amount === 1.62}
 				class={`p-4 h-fit w-fit font-black text-lg cursor-pointer ${
-					amount === 50 ? 'bg-primary' : ''
+					amount === 1.62 ? 'bg-primary' : ''
 				} `}
-				on:click={() => (amount = 50)}
+				on:click={() => amount = 1.62}
 			>
 				50 جنيه
 			</button>
 			<button
-				class:text-black={amount === 100}
+				on:click={() => {
+					amount = 3.24;
+				}}
+				class:text-black={amount === 3.24}
 				class={`p-4 h-fit w-fit font-black text-lg cursor-pointer ${
-					amount === 100 ? 'bg-primary' : ''
+					amount === 3.24 ? 'bg-primary' : ''
 				} `}
-				on:click={() => (amount = 100)}
+				on:click={() => amount = 3.24}
 			>
 				100 جنيه
 			</button>
