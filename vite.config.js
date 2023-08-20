246ec2e40a86dd14bb4714f539ebeb4c30a77ba7
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-08-20 23:34:30 +0300

    fix jsconfig

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { partytownVite } from '@builder.io/partytown/utils'
 import { join } from 'path'
+import path from "path"
 
 export default defineConfig({
+	resolve: {
+        alias: {
+            '$lib': path.resolve('./src/lib/'),
+            '$utils': path.resolve('./src/utils'),
+        },
+    },
 	plugins: [
 		sveltekit(),
 		partytownVite({
