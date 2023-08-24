a468b8c8342ebb23ccc1ba68eb6e43d3c8b27ebc
Abdelra7man1998 <abdelrahman.elmorshedy98@gmail.com>
2023-08-25 00:47:36 +0300

    circle category

--- src/lib/Components/Text/Category.svelte
+++ src/lib/Components/Text/Category.svelte
@@ -1,4 +1,4 @@
 <div class="flex gap-3 items-center h-fit">
-    <div class="w-4 h-4 bg-red rounded-3xl" />
+    <div class="w-4 h-4 bg-red rounded-full" />
     <span class="whitespace-nowrap w-full text-text"><slot /></span>
 </div>
