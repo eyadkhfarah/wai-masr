eed8ca9c47c83cfbfb109d7f3dce9351c8c6e1bb
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-09-06 01:14:35 +0300

    add manager tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,10 +28,39 @@
 		};
 	</script>
 
+	<!-- Google Tag Manager -->
+	<!-- Google Tag Manager -->
+	<!-- Google Tag Manager -->
+	<!-- Google Tag Manager -->
+	<script>
+		(function (w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-K74BSCHL');
+	</script>
+	<!-- End Google Tag Manager -->
+
 	<GoogleAnalytics />
 
 	{@html '<script>' + partytownSnippet() + '</script>'}
 </svelte:head>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+	<iframe
+		src="https://www.googletagmanager.com/ns.html?id=GTM-K74BSCHL"
+		height="0"
+		width="0"
+		style="display:none;visibility:hidden"
+	/>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <Header />
 
